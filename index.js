47cbc64ae576e89ddd9d5ac9ff791195f78078f3
muratozkol <72967829+muratozkol@users.noreply.github.com>
2024-05-29 11:11:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-function createScroller(fromId, toId) {
-  return function() { 
-      $(fromId).click(function(e) {
-          e.preventDefault(); 
-          $('html,body').stop().animate({
-              scrollTop: $(toId).offset().top
-          }, 700);
-      });
-  };
-}
-
 $(document).ready(function() {
+  function createScroller(fromId, toId) {
+    return function() { 
+      $(fromId).click(function(e) {
+        e.preventDefault(); 
+        $('html,body').stop().animate({
+          scrollTop: $(toId).offset().top
+        }, 700);
+      });
+    };
+  }
+
   createScroller('#about-nav', '#about-section')();
   createScroller('#projects-nav', '#projects-section')();
   createScroller('#contact-nav', '#contact-section')();
   createScroller('#home-nav','#home')();
 
   $('.nav a').on('click', function() {
-      $('.navbar-toggle').click();
+    $('.navbar-toggle').click();
   });
 
   for (let i = 1; i <= 16; i++) {
-      let modalClone = $('#projectModalTemplate').clone();
-      modalClone.attr('id', `projectModal${i}`);
-      modalClone.find('.modal-title').text(`Project ${i}`);
-      modalClone.find('img').attr('src', `Projects/${i}.png`);
-      modalClone.find('p').text(`Description for Project ${i}`);
-      $('body').append(modalClone);
-
-      // Modalı tetikleyen imajlara data-target ekleyelim
-      $(`.project img[src='${i}.png']`).attr('data-target', `#projectModal${i}`);
+    let modalClone = $('#projectModalTemplate').clone();
+    modalClone.attr('id', `projectModal${i}`);
+    modalClone.find('.modal-title').text(`Project ${i}`);
+    modalClone.find('img').attr('src', `${i}.png`);  // Görseller HTML dosyasıyla aynı dizinde
+    modalClone.find('p').text(`Description for Project ${i}`);
+    $('body').append(modalClone);
   }
+
+  $('.project img').each(function(index) {
+    $(this).attr('data-target', `#projectModal${index + 1}`);
+  });
 });
