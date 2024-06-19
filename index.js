6e99f24632b8fc0eb0e04a47f2a99cfb9bf0aa06
muratozkol <72967829+muratozkol@users.noreply.github.com>
2024-06-19 17:53:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-$(document).ready(function() {
-  function createScroller(fromId, toId) {
-    return function() { 
+function createScroller(fromId, toId) {
+  scrollFunc = function() { 
       $(fromId).click(function(e) {
-        e.preventDefault(); 
-        $('html,body').stop().animate({
-          scrollTop: $(toId).offset().top
-        }, 700);
+          e.preventDefault(); 
+          $('html,body').stop().animate({
+              scrollTop: $(toId).offset().top
+          }, 700);
       });
-    };
-  }
+  };
+  return(scrollFunc);
+};
 
+$(document).ready(function() {
   createScroller('#about-nav', '#about-section')();
   createScroller('#projects-nav', '#projects-section')();
   createScroller('#contact-nav', '#contact-section')();
   createScroller('#home-nav','#home')();
-
-  $('.nav a').on('click', function() {
-    $('.navbar-toggle').click();
+  
+  $('.nav a').on('click', function(){
+      $('.navbar-toggle').click();
   });
 
   for (let i = 1; i <= 16; i++) {
-    let modalClone = $('#projectModalTemplate').clone();
-    modalClone.attr('id', `projectModal${i}`);
-    modalClone.find('.modal-title').text(`Project ${i}`);
-    modalClone.find('img').attr('src', `${i}.png`);  // Görseller HTML dosyasıyla aynı dizinde
-    modalClone.find('p').text(`Description for Project ${i}`);
-    $('body').append(modalClone);
+      let modalClone = $('#projectModalTemplate').clone();
+      modalClone.attr('id', `projectModal${i}`);
+      modalClone.find('.modal-title').text(``);
+      modalClone.find('img').attr('src', `Projects/${i}.png`);
+      modalClone.find('p').text(``);
+      $('body').append(modalClone);
   }
-
-  $('.project img').each(function(index) {
-    $(this).attr('data-target', `#projectModal${index + 1}`);
-  });
 });
+
